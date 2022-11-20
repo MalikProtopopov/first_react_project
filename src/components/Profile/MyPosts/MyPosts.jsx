7d6f78665da846411202f7c1129753a5c54bf17d
Malik Pro <magamantium@gmail.com>
2022-11-20 19:29:53 +0300

    Got through 19-23 lessons on IT-kam

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
+
 const MyPosts = () => {
     return  <div>
         <div className={s.myPosts}>
